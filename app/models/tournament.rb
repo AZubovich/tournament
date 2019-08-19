594692a758ed2e4f84228557030ec7700ccfde1b
AZubovich <zubovich.200god@gmail.com>
2019-08-19 15:10:10 +0300

    create tournament model

---
+++ app/models/tournament.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: tournaments
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  name        :string
+#  prize       :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint
+#
+# Indexes
+#
+#  index_tournaments_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+
+class Tournament < ApplicationRecord
+  belongs_to :user
+end
---
+++ spec/factories/tournaments.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: tournaments
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  name        :string
+#  prize       :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint
+#
+# Indexes
+#
+#  index_tournaments_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+
+FactoryBot.define do
+  factory :tournament do
+    name { "MyString" }
+    description { "MyText" }
+    prize { 1 }
+    user { nil }
+  end
+end
