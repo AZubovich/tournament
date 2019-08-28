739fda167f6686c9e6dcc04a6eaef5432a4729aa
AZubovich <zubovich.200god@gmail.com>
2019-08-28 15:40:21 +0300

    add players query and test

---
+++ app/graphql/queries/players.rb
@@ -0,0 +1,18 @@
+module Queries
+  class Players < Queries::BaseQuery
+    description 'Get all players from current tournament '
+    
+    argument :id, ID, required: true
+
+    type [Types::PlayerType], null: true
+
+    def resolve(id:)
+      tour = Tournament.find_by(id: id)
+      if tour.kind == 'Regular'
+        Player.where(tournament_id: id).order(:points,:nick_name)
+      else
+        Player.where(tournament_id: id)
+      end
+    end
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,6 +6,7 @@ module Types
     field :find_tournament, resolver: Queries::FindTournament
     field :user_tournaments, resolver: Queries::UserTournaments
     field :player_tournaments, resolver: Queries::PlayerTournaments
+    field :players, resolver: Queries::Players
     field :profile, UserType, null: true,
 
     description: 'profile using'
